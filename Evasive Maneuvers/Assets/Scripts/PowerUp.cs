2dfdd25c5d0ffa42ea276165fa813122f7ad9a5a
Victor Hsiao <vwhsiao@uci.edu>
2016-02-17 17:30:45 -0800

    Powerup code. Really generic fire picture as a placeholder for art Will be changing it to match the icicle and snowball ammo stuff.

---
+++ Evasive Maneuvers/Assets/Scripts/PowerUp.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum PowerUpType { SpeedUp, ClearScreen }
+
+public class PowerUp : MonoBehaviour {
+
+
+    public PowerUpType type = PowerUpType.SpeedUp;
+    [Range(0.0f, 50.0f)]
+    public float timeTilDelete = 5.0f;
+    
+	// Use this for initialization
+	void Start () {
+        Invoke("deleteSelf", timeTilDelete);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        
+        if (col.gameObject.tag == "Player")
+        {
+            if (type == PowerUpType.ClearScreen)
+            {
+                GameObject[] enemies = GameObject.FindGameObjectsWithTag("enemy");
+                for (int i = 0; i < enemies.Length; i++)
+                {
+                    Destroy(enemies[i].gameObject);
+                }
+                Destroy(this.gameObject);
+            }
+            else if (type == PowerUpType.SpeedUp)
+            {
+                StartCoroutine(SpeedUpBoost());
+                
+            }
+        }
+    }
+
+    public IEnumerator SpeedUpBoost()
+    {
+        GameObject player = GameObject.Find("Player");
+        player.GetComponent<Player>().moveSpeed *= 2;
+        
+        yield return new WaitForSeconds(5f);
+        player.GetComponent<Player>().moveSpeed /= 2;
+    }
+
+    void deleteSelf()
+    {
+        Destroy(this.gameObject);
+    }
+}
