d1e1ab94e9b5e267d3a09541cf712502deef1bbe
Faye Jao <yjao@uci.edu>
2016-02-26 17:53:39 -0800

    * added enemy group animations to some, not working yet * fixed prefabs so they don't push each other

---
+++ Evasive Maneuvers/Assets/Scripts/EnemyAnimationGroup.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAnimationGroup : MonoBehaviour
+{
+    public enum Rotation { NONE, CLOCKWISE, COUNTERCLOCKWISE };
+    public Rotation rotation = Rotation.NONE;
+    public int rotationDegrees = 30;
+    private float deltaRotationZ;
+
+	void Start()
+    {
+	    switch (rotation)
+        {
+        case Rotation.CLOCKWISE:
+            deltaRotationZ = rotationDegrees;
+            break;
+        case Rotation.COUNTERCLOCKWISE:
+            deltaRotationZ = -rotationDegrees;
+            break;
+        case Rotation.NONE:
+        default:
+            deltaRotationZ = 0f;
+            break;
+        }
+	}
+	
+	void Update()
+    {
+        Quaternion newRotation = transform.rotation;
+        newRotation.z += deltaRotationZ;
+        Debug.Log (newRotation.z);
+        transform.rotation = newRotation;
+    }
+}
